394e9c7c4ba4ae722701434b2e6c2819e570ae90
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2022-09-15 16:54:31 -0300

    Inicializado projeto fuse

---
+++ app/src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,12 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'home',
+        title: 'Home',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/home',
+    },
+];
