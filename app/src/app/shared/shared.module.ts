3672925718f51dfc50c509d75fdf894c304b40e9
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2022-09-16 10:17:35 -0300

    Melhoras na home

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,20 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslocoModule } from '@ngneat/transloco';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        TranslocoModule,
-    ],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, TranslocoModule],
     exports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
         TranslocoModule,
-    ]
+
+        MatButtonModule,
+        MatIconModule,
+    ],
 })
-export class SharedModule
-{
-}
+export class SharedModule {}
